6a96ef5bc1e720df8061125c1064fefb82cde1e9
SKNNT <antosha.skvortsov.02@gmail.com>
2024-10-05 23:38:39 +0300

    fix

--- ESP32_C3_OLED/OLED_HelloWorld/README.txt
+++ ESP32_C3_OLED/OLED_HelloWorld/README.txt
@@ -1,3 +1,3 @@
 1. https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
 2. don't forget to install lib: U8g2
-3. uncomment: U8G2_SSD1306_72X40_ER_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+3. uncomment: U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
---
+++ ESP32_C3_OLED/sin_display/sketch_oct5a/sketch_oct5a.ino
@@ -0,0 +1,38 @@
+#include <Arduino.h>
+#include <U8g2lib.h>
+
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C
+#include <Wire.h>
+#endif
+
+#define SDA_PIN 5
+#define SCL_PIN 6
+
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+
+void setup(void) {
+  Wire.begin(SDA_PIN, SCL_PIN);
+  u8g2.begin();
+}
+
+void loop(void) {
+  drawSineWave();
+  delay(20);
+}
+
+void drawSineWave() {
+  static float x = 0;
+  u8g2.clearBuffer(); 
+  for (int i = 0; i < 128; i++) {
+    int y = 35 + 15 * sin((x + i) * 0.1);
+    u8g2.drawPixel(i, y);
+  }
+
+  u8g2.sendBuffer(); 
+  x += 1; //animation effect
+
+  delay(5);
+}
